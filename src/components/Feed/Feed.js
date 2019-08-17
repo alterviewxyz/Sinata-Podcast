f6ffb998d7c186d3691c0272a3f13a88b0639b9d
Mehrad Rousta <mehrad77@gmail.com>
2019-08-17 17:54:52 +0430

    redesign cards

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import moment from 'moment-jalaali';
-moment.loadPersian();
+moment.loadPersian({usePersianDigits: true});
 import { Link } from 'gatsby';
 import type { Edges } from '../../types';
 import styles from './Feed.module.scss';
@@ -17,25 +17,29 @@ const Feed = ({ edges }: Props) => {
       {edges.map((edge) => (
         <div className={styles['feed__item']} key={edge.node.fields.slug}>
           <div className={styles['feed__item-part']}>
-            <div className={styles['feed__item-meta']}>
-              <span className={styles['feed__item-meta-category']}>
-                {/* <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.category}</Link> */} 
+           <div className={styles['feed__item-meta']}>
+              <div className={styles['feed__item-meta-category']}>
+                {/* <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.category}</Link>  */}
                 {/* // fixme */}
-                <span className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.number}</span>
-              </span>
-              <span className={styles['feed__item-meta-divider']} />
+                <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>
+                  <div className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.number}</div>
+                </Link>
+              </div >
+              {/* TODO: show it on mobile */}
               <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
-                {moment(edge.node.frontmatter.date).format('jMMMM jYYYY')}
+                {moment(edge.node.frontmatter.date).format('jMMMM jYY')}
               </time>
             </div>
+          </div>
+          <div className={styles['feed__item-part']}>
+            <img src={edge.node.frontmatter.cover}/>
+          </div>
+          <div className={styles['feed__item-part']}>
             <h2 className={styles['feed__item-title']}>
               <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
             </h2>
             <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
-            <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>مشاهده</Link>
-          </div>
-          <div className={styles['feed__item-part']}>
-            <img src={edge.node.frontmatter.cover}/>
+            {/* <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>مشاهده</Link> */}
           </div>
         </div>
       ))}
